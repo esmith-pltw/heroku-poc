0ab6f7d611f92d7c4d812af1e3ffc0b7d8ffde03
Estel Smith <esmith@pltw.org>
2015-12-09 16:11:30 -0500

    add simple migration

---
+++ app/DoctrineMigrations/Version20151209160107.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+class Version20151209160107 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $table = $schema->createTable('users');
+
+        $table->addColumn('id', 'integer');
+        $table->setPrimaryKey(['id']);
+
+        $table->addColumn('username', 'string');
+        $table->addUniqueIndex(['username']);
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $schema->dropTable('users');
+    }
+}
